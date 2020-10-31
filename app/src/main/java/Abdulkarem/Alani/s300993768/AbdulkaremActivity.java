ccadd6c21ec3db07cb8197793e14506c1bf1325b
Karem <karemalani2402@yahoo.com>
2020-10-31 08:43:33 -0400

    Skeleton

---
+++ app/src/main/java/Abdulkarem/Alani/s300993768/AbdulkaremActivity.java
@@ -0,0 +1,14 @@
+package Abdulkarem.Alani.s300993768;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//Abdulkarem Alani 300993768
+public class AbdulkaremActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/Abdulkarem/Alani/s300993768/AlaniActivity.java
@@ -0,0 +1,14 @@
+package Abdulkarem.Alani.s300993768;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlaniActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alani);
+    }
+}
